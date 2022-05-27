2f96b29ce426b8f23f8ec7e19c1157e4d4115b6f
Devansh Khandelwal <85142762+devanshkhandelwal@users.noreply.github.com>
2022-05-27 15:19:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-\import React from "react"
+import React from "react"
 import ReactDOM from "react-dom"
 
-const elem = (
-    <div>
-    <img src="./react-logo.png" width = "40px"></img>
-    <h1>Fun facts about React</h1>
-     <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on Github</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-     </ul>
-    </div>
-)
 
-ReactDOM.render(elem, document.getElementById("root"))
+function temporaryName() {
+    return (
+        <div>
+            <img src="./react-logo.png" width="40px" />
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    )
+}
+
+ReactDOM.render(temporaryName(), document.getElementById("root"))
